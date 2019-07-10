c49c1bc8a791fc107622df1c51502bd18d7d0047
José Manuel Sánchez <buscarini@gmail.com>
2019-07-10 14:09:13 +0200

    Fixes

--- videoTheater.js
+++ videoTheater.js
@@ -1,15 +1,41 @@
-var sourcesCollection = document.getElementsByTagName("source");
+function findSources(node) {
+	var sourcesCollection = node.getElementsByTagName("source");
+	var videoSources = Array.from(sourcesCollection);
 
-var videoSources = Array.from(sourcesCollection);
+	return videoSources
+	.filter(source => {
+		return source.src.includes("m3u");
+	})
+	.map(source => {
+		return {
+			node: source.parentNode,
+			url: source.src
+		};
+	});
+};
 
-let sources = videoSources.filter(source => {
-	return source.src.includes("m3u");
-});
+function findVideos(node) {
+	var videosCollection = node.getElementsByTagName("video");
+	var videoNodes = Array.from(videosCollection);
 
-if (sources.length == 0) {
+	return videoNodes.filter(video => {
+		return video.src.includes("m3u");
+	})
+	.map(source => {
+		return {
+			node: source,
+			url: source.src
+		};
+	});
+};
+
+
+let objs = findSources(document).concat(findVideos(document));
+
+if (objs.length == 0) {
 	console.log("No videos found");
 } else {
-	sources[0].parentNode.pause();
-	let url = sources[0].src;
-	window.location.href = url;
+	let v = objs[0];
+	v.node.pause();
+	window.location.href = v.url;
 }
